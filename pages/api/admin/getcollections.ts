1581803a516108c2a587ce42c261636894640abf
Samarpit Santoki <79189337+SamarpitSantoki@users.noreply.github.com>
2022-09-19 16:30:00 +0530

    Update getcollections.ts

--- pages/api/admin/getcollections.ts
+++ pages/api/admin/getcollections.ts
@@ -6,6 +6,6 @@ export default async function handler(req, res) {
 
   const data = await mongoose.connection.db.listCollections().toArray();
   // res.status(200).json(typeof data[0].name);
-  const collections = data.map((collection) => collection.name);
+  const collections = ['users','products','orders',"categories"];
   res.status(200).json(collections);
 }
